7262a0bb8d983dc6531c90134981b4b1bbc5d025
Vikram Kumar <vikramkbgs@gmail.com>
2024-03-16 18:46:07 +0530

    Loop

--- data.js
+++ data.js
@@ -1,3 +1,3 @@
-module.exports = { date: '2024-03-12T18:46:06+05:30', code: '[object Object]', functionCode: 'function function3() {
-	// Function body
-}' };
+module.exports = { date: '2024-03-16T18:46:07+05:30', code: 'for (let i = 0; i < 10; i++) {
+	// Loop body
+}', functionCode: '' };
