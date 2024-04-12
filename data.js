f6db9bb2ce023029bae3cdac97128ff8a22848a5
Vikram Kumar <vikramkbgs@gmail.com>
2024-04-12 18:46:25 +0530

    Conditional Statement

--- data.js
+++ data.js
@@ -1,3 +1,3 @@
-module.exports = { date: '2024-03-15T18:46:25+05:30', code: 'else (condition) {
+module.exports = { date: '2024-04-12T18:46:25+05:30', code: 'if (condition) {
 	// Statement
 }', functionCode: '' };
