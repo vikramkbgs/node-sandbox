ac9883bb0d5a9941902af9c76f0b862d3868987e
vikramkbgs <vikramkbgs@gmail.com>
2024-05-01 09:53:46 +0530

    socket programing saving message for all user

---
+++ 7. Loading previous messages/config.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+
+export const connect = async()=>{
+    await mongoose.connect("mongodb://127.0.0.1:27017/chatApp", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+    console.log("Db is connected");
+}
---
+++ 7. Loading previous messages/server.js
@@ -0,0 +1,62 @@
+import express from 'express';
+import { Server } from 'socket.io';
+import cors from 'cors';
+import http from 'http';
+import { connect } from './config.js';
+import { chatModel } from './chat.schema.js';
+
+const app = express();
+
+// 1. Creating server using http.
+const server = http.createServer(app);
+
+// 2. Create socket server.
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ["GET", "POST"]
+    }
+});
+
+// 3. Use socket events.
+
+io.on('connection', (socket) => {
+    console.log("Connection is established");
+
+    socket.on("join", (data) => {
+        socket.username = data;
+        // send old messages to the clients.
+        chatModel.find().sort({ timestamp: 1 }).limit(50)
+            .then(messages => {
+                socket.emit('load_messages', messages);
+            }).catch(err => {
+                console.log(err);
+            })
+    });
+
+    socket.on('new_message', (message) => {
+        let userMessage = {
+            username: socket.username,
+            message: message
+        }
+
+        const newChat = new chatModel({
+            username: socket.username,
+            message: message,
+            timestamp: new Date()
+        });
+        newChat.save();
+
+        // broadcast this message to all the clients.
+        socket.broadcast.emit('broadcast_message', userMessage);
+    })
+
+    socket.on('disconnect', () => {
+        console.log("Connection is disconnected");
+    })
+});
+
+server.listen(3000, () => {
+    console.log("App is listening on 3000");
+    connect();
+})
