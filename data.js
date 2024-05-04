7fa6d2342a800f87566b1eeb92132035e3e87847
Vikram Kumar <vikramkbgs@gmail.com>
2024-05-04 18:27:30 +0530

    Class

--- data.js
+++ data.js
@@ -1,3 +1,3 @@
-module.exports = { date: '2024-05-27T18:27:30+05:30', code: 'else (condition) {
-	// Statement
+module.exports = { date: '2024-05-04T18:27:30+05:30', code: 'class Class2 {
+	// Class body
 }', functionCode: '' };
