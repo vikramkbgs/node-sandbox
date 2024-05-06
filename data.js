1576aa67a4380280e7817900c9ecf4029f4e671e
Vikram Kumar <vikramkbgs@gmail.com>
2024-05-06 18:31:37 +0530

    Variable Declaration

--- data.js
+++ data.js
@@ -1,3 +1,1 @@
-module.exports = { date: '2024-05-14T18:31:37+05:30', code: 'if (condition) {
-	// Statement
-}', functionCode: '' };
+module.exports = { date: '2024-05-06T18:31:37+05:30', code: 'var variable77 = 14;', functionCode: '' };
